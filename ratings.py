9df12a55e3245d3b83151312c15fe1638c0b254d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-02 20:57:19 -0700

    Line 29 - cant convert int - fix int bug

--- ratings.py
+++ ratings.py
@@ -15,7 +15,25 @@ def restaurant_rating_printer(file):
 
 		#sorted_restaurants = sorted(restaurant_rating_dict.items())
 	# print (sorted(restaurant_rating_dict.items()))
-	for restaurant,score in sorted(restaurant_rating_dict.items()):
+	for restaurant, score in sorted(restaurant_rating_dict.items()):
 		print ("{} is rated at {}.".format(restaurant, score))
 
-restaurant_rating_printer("scores.txt")
+	return restaurant_rating_dict
+
+def add_ratings_scores(rest_dict):
+	"""takes user input for a retaurant and it's score and either update or add"""
+
+
+	new_restaurant = input("What restaurant did you visit? ")
+	new_score = input("What score from 1 to 5 would you give it? ")
+	new_score = int(new_score)
+	if new_restaurant in rest_dict: 
+		rest_dict[new_restaurant] += new_score
+
+	else: 
+		rest_dict[new_restaurant] = new_score
+
+	print(rest_dict)
+
+r = restaurant_rating_printer("scores.txt")
+add_ratings_scores(r)
